290c15981436137b64d5ceb7a8b85dc20a2b50d0
vladica1984 <vladabeba84@gmail.com>
2017-01-26 20:24:19 +0100

    Domaci zadatak termin12 closes #1

---
+++ app/src/main/java/rs/aleph/android/example12/activities/ThirdActivity.java
@@ -0,0 +1,49 @@
+package rs.aleph.android.example12.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import rs.aleph.android.example12.R;
+
+/**
+ * Created by Alen on 26-Jan-17.
+ */
+
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
